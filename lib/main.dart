70fb019f2dbc1fb23527d25049b2eb0be1c4ea8e
haroonkhan9426 <haroonkhan9426@gmail.com>
2020-04-16 23:55:48 -0700

    FCM Package added

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
