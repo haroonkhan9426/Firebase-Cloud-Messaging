1ba86198a304797f90251e23010dd097d2c49894
haroonkhan9426 <haroonkhan9426@gmail.com>
2020-04-17 19:45:10 -0700

    Basic Notifications done and Tested on Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+
+Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) {
+  if (message.containsKey('data')) {
+    // Handle data message
+    final dynamic data = message['data'];
+  }
+
+  if (message.containsKey('notification')) {
+    // Handle notification message
+    final dynamic notification = message['notification'];
+  }
+}
+
 
 class MyApp extends StatefulWidget {
   // This widget is the root of your application.
@@ -9,11 +23,32 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  FirebaseMessaging _fcm = FirebaseMessaging();
 
   @override
   void initState() {
-    // TODO: implement initState
+    _configureFCM();
     super.initState();
+  }
+
+  _configureFCM() {
+    _fcm.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        // Called directly when the app is in foreground
+        print('onMessage: $message');
+      },
+      onResume: (Map<String, dynamic> message) async {
+        // Called when the app is in background but not terminated and user
+        // taps on the notification in the notifications tray.
+        print('onResume: $message');
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        // Called when the app is terminated and user taps on the notification
+        // in the notification tray.
+        print('onLaunch: $message');
+      },
+      onBackgroundMessage: myBackgroundMessageHandler
+    );
   }
 
   @override
